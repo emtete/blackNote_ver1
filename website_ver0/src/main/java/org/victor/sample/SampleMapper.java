a95f813f6dae2e4f58c93bc2b7c3a6497c2db96f
victor <victor_77@naver.com>
2019-03-12 10:39:59 +0900

    Html/Css Create( Bootstrap )

---
+++ website_ver0/src/main/java/org/victor/sample/SampleMapper.java
@@ -0,0 +1,11 @@
+package org.victor.sample;
+
+import org.apache.ibatis.annotations.Select;
+
+public interface SampleMapper {
+
+	@Select("SELECT sysdate FROM dual")
+	public String getTime();
+	
+	public String getTime2();
+}
